9e762eaf7f73428f8334ef1ebdcb8c03f20f6fdc
Derek Jesus Garcia <derekgar@fb.com>
2019-07-01 17:01:58 -0700

    8/8 done

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -63,12 +63,12 @@ public final class R {
         public static final int all = 0x7f07001c;
         public static final int basic = 0x7f07001f;
         public static final int chains = 0x7f070028;
-        public static final int none = 0x7f070052;
-        public static final int packed = 0x7f070057;
-        public static final int parent = 0x7f070058;
-        public static final int spread = 0x7f070076;
-        public static final int spread_inside = 0x7f070077;
-        public static final int wrap = 0x7f070091;
+        public static final int none = 0x7f070053;
+        public static final int packed = 0x7f070058;
+        public static final int parent = 0x7f070059;
+        public static final int spread = 0x7f070077;
+        public static final int spread_inside = 0x7f070078;
+        public static final int wrap = 0x7f070094;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActvity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActvity.java
@@ -2,23 +2,43 @@ package com.codepath.apps.restclienttemplate;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActvity extends AppCompatActivity {
     private TwitterClient client;
+    TweetAdapter tweetAdapter;
+    ArrayList<Tweet> tweets;
+    RecyclerView rvTweets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
         client = TwitterApp.getRestClient(this);
+        //find recycler view
+        rvTweets= (RecyclerView) findViewById(R.id.rvTwitter);
+        //init the array list from this data source
+        tweets= new ArrayList<>();
+        //countrstuct the adapter from this datasource
+        tweetAdapter = new TweetAdapter(tweets);
+        //RecyclerView setup (layout manager,use adapter)
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(tweetAdapter);
+
+
         populateTimeline();
 
     }
@@ -26,14 +46,27 @@ public class TimelineActvity extends AppCompatActivity {
         client.getHomeTimeline(new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                Log.d("Twitter Client",response.toString());
+                //    Log.d("Twitter Client ",response.toString());
+                //iterate through the Json Array
+                // for each entry deserialize the JSON object
+                for (int i = 0; i < response.length(); i++) {
+                    // convert each object to a tweet model
+                    // ad that tweet model to our data source
+                    // notify the adapater that we changed
+                    try {
+                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
+                        tweets.add(tweet);
+                        tweetAdapter.notifyItemInserted(tweets.size() - 1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 Log.d("Twitter Client ",response.toString());
             }
-
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 Log.d("Twitter Client",responseString);
