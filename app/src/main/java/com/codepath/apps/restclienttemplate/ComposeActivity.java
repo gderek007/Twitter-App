1074090078b2b9ecb35ee4d6a082b95a1152da62
Derek Jesus Garcia <derekgar@fb.com>
2019-07-02 17:46:00 -0700

    finished tweeting

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -0,0 +1,98 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcels;
+
+import cz.msebera.android.httpclient.Header;
+
+public class ComposeActivity<button> extends AppCompatActivity  {
+    public static final String RESULT_TWEET= "result_tweet";
+    Button button;
+    TextView compose;
+    TwitterClient client;
+    TextView charView;
+    EditText edits;
+    public int char_left;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+        button = findViewById(R.id.btnSend);
+        compose = findViewById(R.id.ivCompose);
+        charView = findViewById(R.id.charView);
+        client=TwitterApp.getRestClient(this);
+        // character view
+//        charView.addTextChangedListener(new TextWatcher()
+//        {
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count)
+//            {int length = s.length();
+//            }
+//
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int aft)
+//            {
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s)
+//            {
+//                // this will show characters remaining
+//                charView.setText(280 - s.toString().length() + "/280");
+//            }
+//        });
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                sendTweet();
+               // onSubmit();
+            }
+        });
+    }
+    public void onSubmit() {
+        // closes the activity and returns to first screen
+        this.finish();
+    }
+
+    private void sendTweet(){
+        client.sendTweet(compose.getText().toString(), new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try{
+                    if (statusCode==200){
+                        JSONObject responseJson = new JSONObject(new String(responseBody));
+                        Tweet resultTweet = Tweet.fromJSON(responseJson);
+
+                        Intent resultData = new Intent();
+                        //wrap parcel
+                        resultData.putExtra(RESULT_TWEET, Parcels.wrap(resultTweet));
+                        setResult(RESULT_OK,resultData);
+                        finish();
+
+                    }
+                }
+                catch(JSONException e){
+                    Log.e("Did not work","Different error codes");
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.e("does not wor",responseBody.toString());
+            }
+        });
+    }
+}
+
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActvity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActvity.java
@@ -1,10 +1,14 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -12,6 +16,7 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
@@ -22,6 +27,10 @@ public class TimelineActvity extends AppCompatActivity {
     TweetAdapter tweetAdapter;
     ArrayList<Tweet> tweets;
     RecyclerView rvTweets;
+    public static final int COMPOSE_TWEET_REQUEST_CODE = 100;
+    public static final String RESULT_TWEET= "result_tweet";
+//    public static final String RESULTS_OK = ;
+//    public static final String RESULT_TWEET = ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +41,45 @@ public class TimelineActvity extends AppCompatActivity {
         rvTweets= (RecyclerView) findViewById(R.id.rvTwitter);
         //init the array list from this data source
         tweets= new ArrayList<>();
-        //countrstuct the adapter from this datasource
+        //construct the adapter from this datasource
         tweetAdapter = new TweetAdapter(tweets);
         //RecyclerView setup (layout manager,use adapter)
         rvTweets.setLayoutManager(new LinearLayoutManager(this));
         rvTweets.setAdapter(tweetAdapter);
-
-
         populateTimeline();
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==COMPOSE_TWEET_REQUEST_CODE && resultCode == RESULT_OK){
+            Tweet resultTweet = Parcels.unwrap(getIntent().getParcelableExtra(RESULT_TWEET));
+            tweets.add(0,resultTweet);
+            tweetAdapter.notifyItemInserted(0);
+            rvTweets.scrollToPosition(0);
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.miCompose:
+                Intent intent_compose = new Intent(TimelineActvity.this,ComposeActivity.class);
+                startActivityForResult(intent_compose, COMPOSE_TWEET_REQUEST_CODE);
+                return true;
+//            case R.id.miProfile:
+//                showProfileView();
+//                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
     private void populateTimeline(){
         client.getHomeTimeline(new JsonHttpResponseHandler(){
